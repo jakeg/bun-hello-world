9c3433249e4973d7fe0e47f8e9c87b11f6ad0009
Jake Gordon <github@jakeg.co.uk>
2024-07-18 07:14:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { env } from 'process'
 
 let count = 0;
-let start = Date.now();
+const start = Date.now();
 
 const server = Bun.serve({
   port: env.PORT || 3000, // Heroku expects your app to bind to env.PORT
@@ -9,7 +9,7 @@ const server = Bun.serve({
     return new Response(`<!doctype html>
       <h1>Welcome to Bun, running on Heroku!</h1>
       <p>You are visitor number ${ ++count } since the last Dyno restart ${ Math.round((Date.now() - start)/1000) }s ago.</p>
-    `);
+    `, { headers: { "Content-Type": "text/html" } } );
   },
 });
 
